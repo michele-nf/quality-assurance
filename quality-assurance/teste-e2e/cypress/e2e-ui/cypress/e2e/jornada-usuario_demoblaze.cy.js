09c22eb54fb4a57f92e7f13916a072d787b003e8
Michele <michele.nascf@gmail.com>
2025-01-07 20:43:43 -0300

    Criar testes: Jornada do usuário Demoblaze - Preencher os dados no formulário de finalização de compra.

--- quality-assurance/teste-e2e/cypress/e2e-ui/cypress/e2e/jornada-usuario_demoblaze.cy.js
+++ quality-assurance/teste-e2e/cypress/e2e-ui/cypress/e2e/jornada-usuario_demoblaze.cy.js
@@ -15,14 +15,23 @@ describe("Jornada do Usuário", () => {
         cy.location("pathname").should("equal", "/prod.html");
         cy.get(".name").should("exist").contains("Samsung galaxy s6");
         cy.get(".price-container").should("exist").contains("$360 *includes tax");
-        cy.get('.col-sm-12 > .btn').click();
-        cy.on('window:alert', (alertText) => {
+        cy.get(".col-sm-12 > .btn").click();
+        cy.on("window:alert", (alertText) => {
             expect(alertText).to.eq('Product added'); // Texto esperado do alerta
         });
-        cy.get('#cartur').click();
+        cy.get("#cartur").click();
         cy.location("pathname").should("equal", "/cart.html");
-        cy.get('.success > :nth-child(2)').should("exist").contains("Samsung galaxy s6");
-        cy.get('.success > :nth-child(3)').should("exist").contains("360");
-        cy.get('#totalp').should("exist").contains("360");
+        cy.get(".success > :nth-child(2)").should("exist").contains("Samsung galaxy s6");
+        cy.get(".success > :nth-child(3)").should("exist").contains("360");
+        cy.get("#totalp").should("exist").contains("360");
+        cy.get(".col-lg-1 > .btn").click();
+        cy.get("#orderModalLabel").should("exist").contains("Place order");
+        cy.get("#name").type("Usuário Jornada Demoblaze", { delay: 0}).should("have.value", "Usuário Jornada Demoblaze");
+        cy.get("#country").type("Brasil").should("have.value", "Brasil");
+        cy.get("#city").type("São Paulo").should("have.value", "São Paulo");
+        cy.get("#card").type("123456789").should("have.value", "123456789");
+        cy.get("#month").type("10").should("have.value", "10");
+        cy.get("#year").type("2010").should("have.value", "2010");
+        cy.contains("Purchase").click();
     })
 })
